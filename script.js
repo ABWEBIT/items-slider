ab73dc85c039084cafd04ec353349902efa82584
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2022-11-23 13:38:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,5 +50,5 @@ class ItemsSlider{
     this.box.style.transform = 'translateX(calc('+percent+'% - '+indent+'px))';
   }
 }
-new ItemsSlider('videos',350);
-new ItemsSlider('movies',250);
+new ItemsSlider('videos',300);
+new ItemsSlider('movies',200);
